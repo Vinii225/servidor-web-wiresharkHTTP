9c9448c257837643ad135ffb018297ebabcf8465
Vinícius Ares <vinicius.ares12@gmail.com>
2025-11-22 22:45:29 -0300

    Ajustes na formatação e melhorias nas mensagens de status do servidor

--- Servidor-Web/webserver.py
+++ Servidor-Web/webserver.py
@@ -10,41 +10,43 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 
 serverPort = 3000
 serverSocket.bind(('', serverPort))
-print('Porta sendo usada: ', serverPort)
+print('Porta sendo usada:', serverPort)
 # INFO: Isso aqui vai abrir 1 conexão.
 serverSocket.listen(1)
 
 #Fill in end 
- 
 while True: 
     # Estabelece a conexão 
-    print('Ready to serve...') 
+    print('\nReady to serve...') 
     connectionSocket, addr = serverSocket.accept()
- 
+
     try: 
         # Recebe a mensagem do cliente (requisição HTTP) 
         message = connectionSocket.recv(1024).decode()
         filename = message.split()[1]   
         f = open(filename[1:]) 
         outputdata = f.read()
- 
+
+        # INFO: caso dê certo, confirmação pelo terminal também
+        print(f'STATUS: 200 OK - Arquivo: {filename[1:]} servido para {addr[0]}')
+
         # Envia a linha de status do cabeçalho HTTP 
         connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
- 
+
         # Envia o conteúdo do arquivo ao cliente 
         for i in range(0, len(outputdata)): 
             connectionSocket.send(outputdata[i].encode()) 
         connectionSocket.send("\r\n".encode()) 
- 
+
         # Fecha a conexão com o cliente 
         connectionSocket.close() 
- 
+
     except IOError: 
+        #INFO: caso dê errado, confirmação pelo terminal também
+        print(f'STATUS: 404 Not Found - Arquivo: {filename[1:]} não encontrado.')
+
         connectionSocket.send('HTTP/1.1 404 Not found\r\n\r\n'.encode())
         connectionSocket.send('<html><body><h1>404 Not Found</h1></body></html>\r\n'.encode())
 
         # INFO: fecha caso tenha erro :v
         connectionSocket.close()
- 
-serverSocket.close() 
-sys.exit()  # Encerra o programa
